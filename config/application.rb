912da1499a80965f86c08210bc7f88c0533f42a1
K-takashii <nanase.sigre.1627.yorushika@gmail.com>
2021-10-19 05:18:43 +0000

    Fix

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,12 @@
 require_relative 'boot'
 
-require 'rails/alls'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Bookers2
+module Bookers2s
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
